25922a4f4f5f0f01cf9038ace77a9c3d7bfeada2
yashGhela <ghelayash@gmail.com>
2026-05-24 20:40:36 +0200

    adding signals

--- scripts/barrier.gd
+++ scripts/barrier.gd
@@ -5,12 +5,13 @@ extends Area3D
 var active_area = false
 @export var is_interactable:=true
 @export var Player:Node3D
-var location
+var location = self.global_position
 
 func _input(event):
 	if active_area and event.is_action_pressed("interact") and is_interactable:
 		print("Interacting")
 		animation_player.play("shake")
+		Signalbus.emit_signal("shakeShelf",location)
 
 
 func _on_area_entered(area: Area3D) -> void:
