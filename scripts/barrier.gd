3e33cb473fbd201a1d97ed67b904f7794275fdde
yashGhela <ghelayash@gmail.com>
2026-05-24 20:31:08 +0200

    barrier is fighting me

--- scripts/barrier.gd
+++ scripts/barrier.gd
@@ -1,13 +1,25 @@
-extends MeshInstance3D
-
+extends Area3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-var active_area = false
 
+var active_area = false
+@export var is_interactable:=true
+@export var Player:Node3D
+var location
 
 func _input(event):
-	if active_area and event.is_action_pressed("interact"):
+	if active_area and event.is_action_pressed("interact") and is_interactable:
 		print("Interacting")
-		#Signalbus.emit_signal("display_dialog", dialog_key)
-		#Signalbus.emit_signal("talking")
-		#
+		animation_player.play("shake")
+		#Signalbus.emit_signal("shelfShaken", location) this will send the location to the boss ai
+		
+
+
+func _on_area_entered(area: Area3D) -> void:
+	print(active_area)
+	active_area=true # Replace with function body.
+
+
+func _on_area_exited(area: Area3D) -> void:
+	print(active_area)
+	active_area=false # Replace with function body.
