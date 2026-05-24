7f6412e2815151960fcaeb49080236f745607d78
yashGhela <ghelayash@gmail.com>
2026-05-24 20:38:40 +0200

    logs now, now it needs to actuallyy work

--- scripts/barrier.gd
+++ scripts/barrier.gd
@@ -11,15 +11,15 @@ func _input(event):
 	if active_area and event.is_action_pressed("interact") and is_interactable:
 		print("Interacting")
 		animation_player.play("shake")
-		#Signalbus.emit_signal("shelfShaken", location) this will send the location to the boss ai
-		
 
 
 func _on_area_entered(area: Area3D) -> void:
+	
+	active_area=true 
 	print(active_area)
-	active_area=true # Replace with function body.
 
 
 func _on_area_exited(area: Area3D) -> void:
+	
+	active_area=false 
 	print(active_area)
-	active_area=false # Replace with function body.
