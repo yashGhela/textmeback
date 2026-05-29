5be27fc8f71efcae6abfca24678ab4277f257025
yashGhela <ghelayash@gmail.com>
2026-05-29 22:55:36 +0200

    too tired, will finish looping it tmrw

--- scripts/boss.gd
+++ scripts/boss.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @export var pathFollow:PathFollow3D
-
+@export var path:Path3D
 
 func _process(delta: float) -> void:
 	pass
--- scripts/follow_path.gd
+++ scripts/follow_path.gd
@@ -3,4 +3,10 @@ extends State
 @export var boss:CharacterBody3D
 
 func physics_update(delta:float):
-	boss.pathFollow.progress+=2.0*delta
+	var totalPathlength = boss.path.curve.get_baked_length()
+	var direction =1.0
+	if boss.pathFollow.progress>=totalPathlength:
+		direction=-1.0
+		boss.pathFollow.progress+=2.0*delta*direction
+	else:
+		boss.pathFollow.progress+=2.0*delta
