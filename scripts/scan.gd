6c85dc5c7420e49827c62c257fc54012dd6ba49d
yashGhela <ghelayash@gmail.com>
2026-05-31 21:56:17 +0200

    tryyna build the scan then return to path

--- scripts/scan.gd
+++ scripts/scan.gd
@@ -1,1 +1,21 @@
 extends State
+
+@export var boss:CharacterBody3D
+
+@export var rotation_speed := 1.5  # radians per second
+@export var rotation_limit := 45
+
+
+var base_rotation_y := 0.0
+var time := 0.0
+
+func enter():
+	print("scaning")
+	var tween = create_tween()
+	tween.set_loops()  # Infinite loop
+	tween.tween_property(boss, "rotation_degrees:y", -45, 2.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(boss, "rotation_degrees:y", 45, 2.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	
+	get_tree().create_timer(3.0).timeout.connect(func():
+		
+		Transitioned.emit(self,"FollowPath"))
