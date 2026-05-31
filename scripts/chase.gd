9a352994144a86d736cc00799731e2cf90596e38
yashGhela <ghelayash@gmail.com>
2026-05-31 22:38:47 +0200

    idk it bugging man fix it

--- scripts/chase.gd
+++ scripts/chase.gd
@@ -1,6 +1,8 @@
 extends State
 
 @export var boss:CharacterBody3D
+@export var move_speed:=3.0
+
 var player:CharacterBody3D
 
 func enter():
@@ -9,4 +11,23 @@ func enter():
 
 
 func physics_update(_delta:float):
-	pass
+	var direction = player.global_position - boss.global_position
+	
+	direction.y=0
+	
+	if direction.length() > 0.01:
+		boss.look_at(boss.global_position - direction, Vector3.UP)
+	
+	if direction.length()<10:
+		if direction.length()<3:
+			Transitioned.emit(self,"Find")
+			
+		else:
+			boss.velocity = direction.normalized() * move_speed
+	
+	else:
+		boss.velocity = Vector3.ZERO
+		
+	if direction.length()>10:
+		Transitioned.emit(self,"Scan")
+		
