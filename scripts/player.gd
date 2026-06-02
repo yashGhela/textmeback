f38afdee2870349f869b5c5fec64b287d5dd1b26
yashGhela <ghelayash@gmail.com>
2026-06-02 22:04:02 +0200

    just gonna remove the idle animamtion for now man

--- scripts/player.gd
+++ scripts/player.gd
@@ -31,6 +31,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		animation_player.play("Armature|idle")
+		#animation_player.play("Armature|idle")
 
 	move_and_slide()
--- scripts/world.gd
+++ scripts/world.gd
@@ -2,5 +2,11 @@ extends Node3D
 
 @onready var player:CharacterBody3D = get_tree().get_first_node_in_group("Player")
 
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("Player")
+
 func _physics_process(delta: float) -> void:
+	if !player:
+		return
 	get_tree().call_group("Boss", "update_target_location",player.global_transform.origin)
