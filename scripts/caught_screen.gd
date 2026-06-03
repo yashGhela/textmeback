0c3cf23f06c66a6c7d500c62f19b86321c33bd1f
yashGhela <ghelayash@gmail.com>
2026-06-03 17:58:15 +0200

    caught screen done

--- scripts/caught_screen.gd
+++ scripts/caught_screen.gd
@@ -1,5 +1,7 @@
 extends Control
 
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://levels/devlab.tscn") # Replace with function body.
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/devlab.tscn")  # Replace with function body.
