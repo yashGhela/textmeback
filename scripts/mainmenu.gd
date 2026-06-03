f519f22017da74ad3c9bc1e8d50e3a8fa4a97bdf
yashGhela <ghelayash@gmail.com>
2026-06-03 17:52:47 +0200

    main menu done

---
+++ scripts/mainmenu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/devlab.tscn") # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()# Replace with function body.
